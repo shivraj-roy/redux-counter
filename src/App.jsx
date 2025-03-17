01f46bcc668f44dacb96bfd013a71d3d6c356faf
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-17 23:53:47 +0530

    ▣ Add Header and Auth components to App...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Counter from "./components/Counter";
-
+import Auth from "./components/Auth";
+import Header from "./components/Header";
 function App() {
-   return <Counter />;
+   return (
+      <>
+         <Header />
+         <Auth />
+         <Counter />
+      </>
+   );
 }
 
 export default App;
