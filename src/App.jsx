e1308cae0c3f405a1ddff571d14623fbe43cceda
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-18 01:49:06 +0530

    ✨ Integrate authentication state management with Redux...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Counter from "./components/Counter";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
+import { useSelector } from "react-redux";
 function App() {
+   const isAuth = useSelector((state) => state.auth.isAuthenticated);
    return (
       <>
          <Header />
-         <Auth />
-         <Counter />
+         {!isAuth && <Auth />}
+         {isAuth && <Counter />}
       </>
    );
 }
